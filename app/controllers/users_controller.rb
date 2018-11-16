894a069d10687e66a9bab11cad37991c4d400945
Edelman <edelmaned@gmail.com>
2018-11-15 16:57:05 -0800

    Create music app with auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      login(user)
+      redirect_to user_url(user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+
+end
